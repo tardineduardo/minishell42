fd514fa72bc566851bfe8fbed6e2ba07bc567d95
Eduardo@AcerLinux <tardineduardo@gmail.com>
2025-02-27 00:41:14 -0300

    Primeira versão

--- src/ft_capture_command.c
+++ src/ft_capture_command.c
@@ -1,13 +1,5 @@
 #include "../include/minishell.h"
 
-
-
-
-
-
-
-//Depois eu vou expandir essa função para pegar heredoc e continuar a preencher 
-//o comando se a linha terminar com |, seguindo a lógica do flowchart
 char *ft_capture_command(void)
 {
 	char	*command_cap;
@@ -19,8 +11,9 @@ char *ft_capture_command(void)
 		return (NULL);
 	while(1)
 	{
-		//capture_heredocs
-		trimmed_cap = ft_strtrim(command_cap, " \t");    // será que eu deveria trim todos os invisible chars?
+		trimmed_cap = ft_strtrim(command_cap, " \t");
+		if (!trimmed_cap)
+			return (NULL);   // será que eu deveria trim todos os invisible chars?
 		if (trimmed_cap[ft_strlen(trimmed_cap) - 1] == '|')
 		{
 			free(trimmed_cap);
