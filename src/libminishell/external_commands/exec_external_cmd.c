318b031bbbf4c30f5f213aa254dbacfaf417c8d2
Luis Claudio De Carvalho Frade <luide-ca@c1r6p3.42sp.org.br>
2025-03-27 16:21:12 -0300

    added the ft that create a copy of t_list ms_env to char **ms_env and plugged it to external command execve, it passed

--- src/libminishell/external_commands/exec_external_cmd.c
+++ src/libminishell/external_commands/exec_external_cmd.c
@@ -1,18 +1,48 @@
 #include "../include/prototype.h"
 
+char **ft_ms_env_arr(t_list **ms_env)
+{
+	t_env_node	*cur_ms_env_node;
+	t_list		*current;
+	char		**ms_env_cpy;
+	char		*temp_key_value;
+	char		*temp_key_sign;
+	int			lst_size;
+	int			i;
+
+	if (!ms_env || !*ms_env)
+    	return (NULL);
+	lst_size = ft_lstsize(*ms_env);
+	ms_env_cpy = (char **)calloc(sizeof(char *), (lst_size + 1));
+	i = 0;
+	current = *ms_env;
+	while (current)
+	{
+		cur_ms_env_node = current->content;
+		temp_key_sign = ft_strjoin(cur_ms_env_node->variable, "=");
+		temp_key_value = ft_strjoin(temp_key_sign, cur_ms_env_node->value);
+		free(temp_key_sign);
+		ms_env_cpy[i] = temp_key_value;
+		i++;
+		current = current->next;
+	}
+	return (ms_env_cpy);
+}
+
 void	exec_external_cmd(t_list **ms_env, t_cmd_node *cmd)
 {
 	char	**cmd_arr;
-	//char	*cmd_path;
+	char	**ms_env_arr;
 
 	cmd_arr = cmd->cmd_arr;
 	cmd_arr = update_cmd_arr(ms_env, cmd_arr);
+	ms_env_arr = ft_ms_env_arr(ms_env);
 	/*
 		Studying about if is necessary to pass environ when calling execve, I understood
 		that if I pass the binary with the right path (or the first arg from execve) it isnt
 		necessary to pass environ (or the third param from execve) TODO: align this with edu
 	*/
-	if (execve(cmd_arr[0], cmd_arr, NULL) == -1)
+	if (execve(cmd_arr[0], cmd_arr, ms_env_arr) == -1)
 	{
 		perror("execve");
 		exit(EXIT_FAILURE);
