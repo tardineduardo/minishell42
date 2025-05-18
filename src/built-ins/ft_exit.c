48287698394ea82b89dee72e5298c14516416b02
Eduardo@AcerLinux <tardineduardo@gmail.com>
2025-05-18 16:08:18 -0300

    reverting "ft_error" PR

--- src/built-ins/ft_exit.c
+++ src/built-ins/ft_exit.c
@@ -17,25 +17,26 @@ int	ft_exit(char **cmd_arr, t_mem **mem)
 	if (cmd_arr[2] != NULL)
 	{
 		ft_dprintf(2, "%s: too many arguments", cmd_arr[0]);
-		ft_clear_mem_and_exit(mem);
+		// ft_clear_mem_and_exit(mem);
 		exit (1);
 	}
 	if (cmd_arr[1] != NULL && ft_isalpha(cmd_arr[1][0]) != 0)
 	{
 		ft_dprintf(2, "%s: numeric argument required", cmd_arr[0]);
-		ft_clear_mem_and_exit(mem);
+		// ft_clear_mem_and_exit(mem);
 		exit (2);
 	}
 	if (cmd_arr[1] != NULL && ft_atoi(cmd_arr[1]) == 0)
 	{
 		ft_dprintf(2, "%s: numeric argument required", cmd_arr[0]);
-		ft_clear_mem_and_exit(mem);
+		// ft_clear_mem_and_exit(mem);
 		exit (0);
 	}
 	else if (cmd_arr[1] != NULL && ft_atoi(cmd_arr[1]) != 0)
 	{
 		exit(ft_atoi(cmd_arr[1]));
-		ft_clear_mem_and_exit(mem);
+		// ft_clear_mem_and_exit(mem);
 	}
+	ft_clear_mem_and_exit(mem);
 	exit(0);
 }
