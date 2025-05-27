c1fdb27471e957a62235d82e9974aebbeceaff67
Luis Claudio De Carvalho Frade <luide-ca@c1r3p3.42sp.org.br>
2025-05-27 17:27:36 -0300

    wip

--- src/ast/free_ast.c
+++ src/ast/free_ast.c
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 12:49:10 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/05/26 23:36:49 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:40:13 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,53 @@ void	free_block_node(void *ptr)
 }
 
 //Free AST memory
+// void	ft_free_ast(t_ast_node *node)
+// {
+// 	t_block_node	*blk;
+// 	t_pipe_info		*pipe;
+// 	t_logical_data	*log;
+// 	t_subshell_data	*sub;
+
+// 	if (!node)
+// 		return ;
+// 	if (node->type == NODE_COMMAND)
+// 	{
+// 		blk = node->block_node;
+// 		if (blk)
+// 			free_block_node(blk);
+// 	}
+// 	else if (node->type == NODE_PIPELINE)
+// 	{
+// 		pipe = node->pipeline;
+// 		if (pipe)
+// 		{
+// 			ft_lstiter(pipe->cmds, (void (*)(void *))ft_free_ast);
+// 			ft_lstclear(&pipe->cmds, NULL);
+// 			free(pipe);
+// 		}
+// 	}
+// 	else if (node->type == NODE_LOGICAL)
+// 	{
+// 		log = node->logical;
+// 		if (log)
+// 		{
+// 			ft_free_ast(log->left);
+// 			ft_free_ast(log->right);
+// 			free(log);
+// 		}
+// 	}
+// 	else if (node->type == NODE_SUBSHELL)
+// 	{
+// 		sub = node->subshell;
+// 		if (sub)
+// 		{
+// 			ft_free_ast(sub->body);
+// 			free(sub);
+// 		}
+// 	}
+// 	free(node);
+// }
+
 void	ft_free_ast(t_ast_node *node)
 {
 	t_block_node	*blk;
@@ -67,7 +114,8 @@ void	ft_free_ast(t_ast_node *node)
 		pipe = node->pipeline;
 		if (pipe)
 		{
-			ft_lstclear(&pipe->cmds, free_block_node);
+			ft_lstiter(pipe->cmds, (void (*)(void *))ft_free_ast);
+			ft_lstclear(&pipe->cmds, NULL);
 			free(pipe);
 		}
 	}
