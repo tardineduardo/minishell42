e3038150ca552d6f164389703af438584e957ec2
Eduardo@42sp <tardineduardo@gmail.com>
2025-06-01 15:50:26 -0300

    new simplified heredoc

--- src/heredoc/heredocs.c
+++ src/heredoc/heredocs.c
@@ -6,7 +6,7 @@
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 20:52:35 by eduribei          #+#    #+#             */
-/*   Updated: 2025/05/31 22:09:12 by eduribei         ###   ########.fr       */
+/*   Updated: 2025/06/01 15:47:36 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,17 @@
 #include "../../include/heredoc.h"
 #include "../../include/expand.h"
 
-static char	*ft_capture_in_interactive_mode(char *prompt)
-{
-	char	*line;
-
-	heredoc_signal();
-	line = readline(prompt);
-	free(prompt);
-	return (line);
-}
-
-static void	ft_free_mem_in_heredoc_child(int fd, char *filepath, t_mem **mem)
-{
-	close(fd);
-	free(filepath);
-	ft_clear_mem_and_exit(mem);
-	return ;
-}
-
-static void	ft_run_heredoc_child(char *filepath, char *delim, t_mem **mem)
+static void	ft_capture_loop(char *delim, int fd, t_mem **mem)
 {
 	char	*line;
 	char	*prompt;
 	char	*temp;
-	int		fd;
 
-	fd = open(filepath, O_WRONLY | O_APPEND);
-	if (fd < 0)
-		exit(EXIT_FAILURE);
 	while (1)
 	{
 		prompt = ft_concatenate("heredoc [", delim, "] > ");
-		line = ft_capture_in_interactive_mode(prompt);
+		line = readline(prompt);
+		free(prompt);
 		if (!line)
 			exit(EXIT_FAILURE);
 		if (ft_strcmp(line, delim) == 0)
@@ -57,7 +36,21 @@ static void	ft_run_heredoc_child(char *filepath, char *delim, t_mem **mem)
 		ft_dprintf(fd, "%s\n", temp);
 		free(temp);
 	}
-	ft_free_mem_in_heredoc_child(fd, filepath, mem);
+	return ;
+}
+
+static void	ft_run_heredoc_child(char *filepath, char *delim, t_mem **mem)
+{
+	int		fd;
+
+	fd = open(filepath, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		exit(EXIT_FAILURE);
+	heredoc_signal();
+	ft_capture_loop(delim, fd, mem);
+	close(fd);
+	free(filepath);
+	ft_clear_mem_and_exit(mem);
 	exit(EXIT_SUCCESS);
 }
 
@@ -67,7 +60,6 @@ char	*ft_heredoc(char *delimiter, t_mem **mem)
 	char		*filename;
 	static int	hd_count_int;
 
-	(void)mem;
 	if (!ft_create_hd_filepath(&hd_count_int, &filename))
 		return (NULL);
 	pid = fork();
