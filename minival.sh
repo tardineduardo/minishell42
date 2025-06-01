bcdfde49e2570a47b16ee8493db7650ee727b111
luis frade <luis.carvalho.frade@gmail.com>
2025-05-31 22:45:27 -0300

    teste

--- minival.sh
+++ minival.sh
@@ -1,4 +1,4 @@
-valgrind 2>valgrindtemp.txt --suppressions=readline.supp --leak-check=full --show-leak-kinds=all ./minishell
+valgrind 2>valgrindtemp.txt --track-fds=yes --suppressions=readline.supp --leak-check=full --show-leak-kinds=all ./minishell
 python3 supp.py valgrindtemp.txt
 cat valgrindtemp.txt 
 rm valgrindtemp.txt
