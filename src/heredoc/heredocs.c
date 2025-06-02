cd8ec96836b6b73e7bd9aeaec7e57cad78be0e4c
luis frade <luis.carvalho.frade@gmail.com>
2025-06-02 20:53:08 -0300

    now crtl+d and crtl+c are working, but need yet to understand the behavior from fds

--- src/heredoc/heredocs.c
+++ src/heredoc/heredocs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredocs.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 20:52:35 by eduribei          #+#    #+#             */
-/*   Updated: 2025/06/01 23:49:32 by eduribei         ###   ########.fr       */
+/*   Updated: 2025/06/02 20:48:27 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,50 +18,31 @@ static void	ft_capture_loop(char *delim, int fd, t_mem **mem)
 {
 	char	*line;
 	char	*prompt;
+
 	while (1)
 	{
-		ft_printf("ft_capture_loop 1\n");
 		prompt = ft_concatenate("heredoc [", delim, "] > ");
-		ft_printf("ft_capture_loop 2\n");
 		line = readline(prompt);
-		line = NULL;
-		ft_printf("ft_capture_loop 3\n");
 		free(prompt);
-		ft_printf("ft_capture_loop 4\n");
 		if (!line)
 		{
-			ft_printf("ft_capture_loop 5\n");
 			if (g_signal == SIGINT)
 			{
-				ft_printf("ft_capture_loop 6\n");
 				ft_clear_mem_and_exit(mem);
-				ft_printf("ft_capture_loop 7\n");
-				ft_printf("EXIT_FAILURE = %i\n", EXIT_FAILURE);
-				exit(EXIT_FAILURE);
+				exit(130);
 			}
-			ft_printf("ft_capture_loop 8\n");
+			exit(0);
 		}
-		ft_printf("ft_capture_loop 9\n");
 		if (ft_strcmp(line, delim) == 0)
 		{
-			ft_printf("ft_capture_loop 10\n");
 			free(line);
-			ft_printf("ft_capture_loop 11\n");
 			break ;
 		}
-		ft_printf("ft_capture_loop 12\n");
 		ft_expand(&line, HEREDOC, mem);
-		ft_printf("ft_capture_loop 13\n");
 		if (!line)
-		{
 			exit(EXIT_FAILURE);
-			ft_printf("ft_capture_loop 15\n");
-		}
-		ft_printf("ft_capture_loop 16\n");
 		ft_dprintf(fd, "%s\n", line);
-		ft_printf("ft_capture_loop 17\n");
 		free(line);
-		ft_printf("se chegou aqui nao deu segfault\n");
 	}
 	return ;
 }
@@ -96,25 +77,15 @@ char	*ft_heredoc(char **delimiter, t_mem **mem)
 	if (pid < 0)
 		return (NULL);
 	if (pid == 0)
-	{
 		ft_run_heredoc_child(filename, *delimiter, mem);
-		ft_printf("ft_heredoc (child) 1\n");
-	}
-	ft_printf("ft_heredoc (parent) 2\n");
 	waitpid(pid, &status, 0);
-	ft_printf("ft_heredoc (parent) 3\n");
 	if (WIFSIGNALED(status) || WEXITSTATUS(status) != EXIT_SUCCESS)
 	{
 		(*mem)->heredoc->errnum = status;
-		ft_printf("ft_heredoc (parent) 4\n");
 		unlink(filename);
-		ft_printf("ft_heredoc (parent) 5\n");
 		free(filename);
-		ft_printf("ft_heredoc (parent) 6\n");
 		return (NULL);
 	}
-	ft_printf("ft_heredoc (parent) 7\n");
 	(*mem)->heredoc->errnum = status;
-	ft_printf("ft_heredoc (parent) 8\n");
 	return (filename);
 }
