53d6adf21b2aaf7ffa49a1da975e9a54d3d210f2
Eduardo@AcerLinux <tardineduardo@gmail.com>
2025-06-06 02:44:42 -0300

    .

---
+++ src/tokenize/tokenize_wildcard.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize_syntax.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 15:49:30 by luide-ca          #+#    #+#             */
+/*   Updated: 2025/05/31 23:38:32 by eduribei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include "../../include/tokenize.h"
+
+#include <sys/types.h>
+#include <dirent.h>
+
+int	ft_wildcards(t_dlist *start, t_dlist *end, char *pattern)
+{
+	DIR *folder;
+
+	folder = opendir(".");
+
+
+
+
+return 1;
+}
