6e88e4a427bf48c2d65fc459b257baf4d985d52f
Eduardo@AcerLinux <tardineduardo@gmail.com>
2025-06-10 14:28:04 -0300

    .

--- include/builtins.h
+++ include/builtins.h
@@ -16,7 +16,7 @@
 # include "./minishell.h"
 # include "../libs/libft/libft.h"
 
-// built-ins
+/// built-ins
 int		ft_env(t_list *envlist);
 int		ft_pwd(t_list **envlist);
 int		ft_echo(char **cmd_arr);
@@ -25,11 +25,11 @@ int		ft_exit(char **cmd_arr, t_mem **mem);
 int		ft_export(t_list **envlist, char *variable_value);
 int		ft_unset(t_list **envlist, char *variable);
 
-// built-ins helpers
+/// built-ins helpers
 bool	ft_is_valid_varname(char *var, char *variable_value);
 int		ft_error_export(char *variable);
 
-//needed for SHELL=minishell
+/// needed for SHELL=minishell
 void	*ft_ms_env_add(t_list **envlist, char *var, char *val);
 char	*ft_get_env_value(t_list **envlist, char *env_variable);
 
